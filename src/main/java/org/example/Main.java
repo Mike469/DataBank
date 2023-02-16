ebf4085dab744ebfd04da8b12c88ac8159bd46ec
Malcolm <johnson.malcolm2003@gmail.com>
2023-02-16 11:21:17 -0600

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
@@ -14,6 +15,18 @@ public class Main {
         System.out.print("Enter your password: \n>");
         String password = scanner.nextLine();
         return new String[]{username, password};
+    }
+
+    public static String[] registerInfo() {
+        System.out.print("Enter your 'preferred' first name: \n>");
+        String firstname = scanner.nextLine();
+        System.out.print("Enter your last name: \n>");
+        String lastname = scanner.nextLine();
+        System.out.print("Enter your email address: \n>");
+        String email = scanner.nextLine();
+        System.out.print("Enter your last name: \n>");
+        String accountPassword = scanner.nextLine();
+        return new String[]{firstname, lastname, email, accountPassword};
     }
     public static void main(String[] args) {
         String loggedIn = "Unsuccessful";
@@ -26,10 +39,16 @@ public class Main {
                     loggedIn = regLog.login(userInfo());
                     break;
                 case "Register":
-                    regLog.registration(userInfo());
+                    regLog.registration(registerInfo());
                     break;
             }
         }
         System.out.println(loggedIn);
+//        System.out.println("------Welcome to Databank!------");
+//        System.out.println("While logged in you can:");
+//        System.out.println("DEPOSIT");
+//        System.out.println("");
+//        System.out.println("");
+
     }
 }
